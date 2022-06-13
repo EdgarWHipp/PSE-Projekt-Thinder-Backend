f4a28b5b2d3a37f135d46e07adac57d322160c24
Daniel Wenger <uihoz@student.kit.edu>
2022-06-13 18:27:46 +0200

    Added TestController

---
+++ src/main/java/com/pse/thinder/backend/restController/TestController.java
@@ -0,0 +1,18 @@
+package com.pse.thinder.backend.restController;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pse.thinder.backend.security.ThinderUserDetails;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		ThinderUserDetails details = (ThinderUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		return String.format("Hello %s", details.getUser().getFirstName());
+	}
+	
+}
