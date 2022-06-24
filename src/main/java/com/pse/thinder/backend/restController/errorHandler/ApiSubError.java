f734c1fc50358d104815b79028148e72d0045ed9
uqjyc <uqjyc@student.kit.edu>
2022-06-24 15:36:28 +0200

    Created exception handling classes

---
+++ src/main/java/com/pse/thinder/backend/restController/errorHandler/ApiSubError.java
@@ -0,0 +1,17 @@
+package com.pse.thinder.backend.restController.errorHandler;
+
+abstract class ApiSubError {
+
+}
+
+class ApiValidationError extends ApiSubError {
+    private String object;
+    private String field;
+    private Object rejectedValue;
+    private String message;
+
+    ApiValidationError(String object, String message) {
+        this.object = object;
+        this.message = message;
+    }
+}
