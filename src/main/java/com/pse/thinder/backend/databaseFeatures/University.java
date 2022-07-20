a4730f5cbbeb14199da160b37f17b6e6cbe5fbfc
uqjyc <uqjyc@student.kit.edu>
2022-07-20 17:13:19 +0200

    Inserted inmemory test db and removed unnecessary code

--- src/main/java/com/pse/thinder/backend/databaseFeatures/University.java
+++ src/main/java/com/pse/thinder/backend/databaseFeatures/University.java
@@ -1,6 +1,7 @@
 package com.pse.thinder.backend.databaseFeatures;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.pse.thinder.backend.databaseFeatures.account.User;
 
@@ -18,6 +19,7 @@ public class University {
     @Column(columnDefinition = "character varying(50) unique not null")
     private String name;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "university", cascade = CascadeType.REMOVE)
     private Set<User> members;
 
--- src/test/java/com/pse/thinder/backend/controllers/UniversityControllerTests.java
+++ src/test/java/com/pse/thinder/backend/controllers/UniversityControllerTests.java
@@ -2,6 +2,7 @@ package com.pse.thinder.backend.controllers;
 
 import java.util.UUID;
 
+import com.pse.thinder.backend.repositories.UniversityRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -25,6 +28,7 @@ import com.pse.thinder.backend.databaseFeatures.University;
 import com.pse.thinder.backend.services.UniversityService;
 
 @ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class UniversityControllerTests {
 
 	@Mock
@@ -32,6 +36,9 @@ class UniversityControllerTests {
 	
 	@InjectMocks
 	UniversityController universityController;
+
+	@Autowired
+	UniversityRepository universityRepository;
 	
 	MockMvc mockMvc;
 	
@@ -61,5 +68,15 @@ class UniversityControllerTests {
 		MockHttpServletResponse result = mockMvc.perform(MockMvcRequestBuilders.get("/university/"+u)).andReturn().getResponse();
 		
 		Assertions.assertThat(result.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+	}
+
+	@Test
+	void testAddUniversity() throws Exception { // todo this is a test of the inmemory db... works...
+		University newUni = new University("KIT");
+		newUni.setStudentMailRegex(".*@student.kit.edu");
+		newUni.setSupervisorMailRegex(".*@kit.edu");
+		universityRepository.save(newUni);
+
+		University uni = universityRepository.findByName("KIT").orElseThrow(() -> new EntityNotFoundException("Test"));
 	}
 }
