62bc3e09ae4b64ea75b12a22032af79ad483bbbb
Daniel Wenger <uihoz@student.kit.edu>
2022-08-07 19:50:06 +0200

    Updated getDegree Request

--- src/main/java/com/pse/thinder/backend/controllers/DegreeController.java
+++ src/main/java/com/pse/thinder/backend/controllers/DegreeController.java
@@ -1,14 +1,10 @@
 package com.pse.thinder.backend.controllers;
 
-import com.pse.thinder.backend.databaseFeatures.Degree;
-import com.pse.thinder.backend.databaseFeatures.InputValidation;
 import com.pse.thinder.backend.services.DegreeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.UUID;
 
 @Controller
@@ -18,15 +14,9 @@ public class DegreeController {
     @Autowired
     private DegreeService degreeService;
 
-    @PostMapping()
-    public void postDegree(@Validated(InputValidation.class) @RequestBody Degree degree){
-        degreeService.addDegree(degree);
-    }
-
-
-    @GetMapping("/university/{university-id}")
-    public ArrayList<Degree> getDegreeByUniversity(@PathVariable("university-id") UUID universityId){
-        return  degreeService.getDegreeByUniversity(universityId);
+    @GetMapping("/{id}")
+    public void postDegree(@PathVariable("id") UUID id){
+        degreeService.getDegreeById(id);
     }
 
 }
--- src/main/java/com/pse/thinder/backend/services/DegreeService.java
+++ src/main/java/com/pse/thinder/backend/services/DegreeService.java
@@ -1,11 +1,11 @@
 package com.pse.thinder.backend.services;
 
+import com.pse.thinder.backend.controllers.errorHandler.exceptions.EntityNotFoundException;
 import com.pse.thinder.backend.databaseFeatures.Degree;
 import com.pse.thinder.backend.repositories.DegreeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.UUID;
 
 @Service
@@ -14,11 +14,7 @@ public class DegreeService {
     @Autowired
     private DegreeRepository degreeRepository;
 
-    public void addDegree(Degree degree){
-        degreeRepository.saveAndFlush(degree);
-    }
-
-    public ArrayList<Degree> getDegreeByUniversity(UUID universityId){
-        return  degreeRepository.findByUniversity(universityId);
+    public Degree getDegreeById(UUID id){
+    	return degreeRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(""));
     }
 }
