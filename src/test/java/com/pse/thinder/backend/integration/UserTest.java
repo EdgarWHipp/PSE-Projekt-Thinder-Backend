d7c171b30fa9f0eb4694646f9eaf222a7e69430d
uihoz <uihoz@student.kit.edu>
2022-08-09 17:35:41 +0200

    UserTest updated

--- src/test/java/com/pse/thinder/backend/integration/UserTest.java
+++ src/test/java/com/pse/thinder/backend/integration/UserTest.java
@@ -25,6 +25,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -32,6 +33,7 @@ import com.icegreen.greenmail.util.GreenMail;
 import com.icegreen.greenmail.util.ServerSetup;
 import com.pse.thinder.backend.databaseFeatures.University;
 import com.pse.thinder.backend.databaseFeatures.account.PlainUser;
+import com.pse.thinder.backend.databaseFeatures.account.Student;
 import com.pse.thinder.backend.databaseFeatures.account.User;
 import com.pse.thinder.backend.repositories.UniversityRepository;
 import com.pse.thinder.backend.repositories.UserRepository;
@@ -48,6 +50,9 @@ class UserTest {
 	
 	@Autowired
 	TestRestTemplate testRestTemplate;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
 	
 	private JacksonTester<User> jacksonTester;
 	
@@ -75,6 +80,8 @@ class UserTest {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		
+		
+		//Create User
 		JSONObject userJson = new JSONObject();
 		userJson.put("role", "USER");
 		userJson.put("firstName", "Bob");
@@ -87,21 +94,35 @@ class UserTest {
 		
 		
 		Assertions.assertThat(userResponseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		
+		//Verify User
 		MimeMessage[] mail = mailServer.getReceivedMessages();
-		System.err.println(mail[0].getContentType());
 		String token = mail[0].getContent().toString().split("\n")[2].trim();
-		System.err.println("-"+token+"-");
 		
 		ResponseEntity<String> verifyResponse = testRestTemplate.getForEntity("/users/verify?token={token}", String.class, token);
 		
 		Assertions.assertThat(verifyResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 		
-		System.err.println(userRepository.findAll().toString());
+		//Get Role
+		ResponseEntity<String> roleResponse = testRestTemplate.withBasicAuth("uihoz@student.kit.edu", "password").getForEntity("/users/current/getRole", String.class);
+		Assertions.assertThat(roleResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+		Assertions.assertThat(roleResponse.getBody()).isEqualTo("\"STUDENT\"");
+		
+		//Get Student
+		ResponseEntity<String> studentResponse = testRestTemplate.withBasicAuth("uihoz@student.kit.edu", "password").getForEntity("/users/current", String.class);
+		JSONObject student = new JSONObject(studentResponse.getBody());
+		
+		Assertions.assertThat(studentResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+		Assertions.assertThat(student.get("firstName")).isEqualTo("Bob");
+		Assertions.assertThat(student.get("lastName")).isEqualTo("Fischer");
+		Assertions.assertThat(student.get("mail")).isEqualTo("uihoz@student.kit.edu");
+		Assertions.assertThat(student.get("password")).isEqualTo(passwordEncoder.encode("password"));
 	}
 	
 	@AfterEach
 	void cleanUp() {
 		universityRepository.deleteAll();
+		userRepository.deleteAll();
 		mailServer.stop();
 	}
 
